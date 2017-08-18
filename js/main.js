194bd663afb0cb87ab095b6154f9fdf56178b50d
pamato <pauloserodio@Paulos-MacBook-Pro.local>
2017-08-18 06:05:11 +0200

    update js file

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
-  $('.navigation-wrapper .blog-button').click(function () {
+  $('.navigation-wrapper .research-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
