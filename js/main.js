686badafe271cc7b907462c4ef6973ca0e145eb4
pamato <pauloadrianoserodio@gmail.com>
2017-08-31 00:33:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
 
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
     $('.panel-cover').addClass('panel-cover')
+    /*$('.panel-cover').addClass('panel-cover--collapsed') */
   }
 
   $('.btn-mobile-menu').click(function () {
