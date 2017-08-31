f4748da8e4ee03101d6497f43d39c139d5a64c81
pamato <pauloserodio@Paulos-MacBook-Pro.local>
2017-08-31 06:24:33 +0200

    opk

--- js/main.js
+++ js/main.js
@@ -23,10 +23,6 @@ $(document).ready(function () {
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-  
-  if (window.location.pathname == '{{ site.url }}/research' {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
