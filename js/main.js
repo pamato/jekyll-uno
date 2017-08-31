da4cd889b7e29f8bfabb1bd0bc572a666ab20e37
pamato <pauloserodio@Paulos-MacBook-Pro.local>
2017-08-31 04:15:43 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -21,10 +21,10 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
   
-  /*if (window.location.pathname !== '{{ site.url }}' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-    $('.panel-cover').addClass('panel-cover')
+  if (window.location.pathname !== '{{ site.url }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+    /*$('.panel-cover').addClass('panel-cover')*/
     $('.panel-cover').addClass('panel-cover--collapsed') 
-  }*/
+  }
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
