032f9b02ed422a58e80b74225b99d33250d90c6b
pamato <pauloserodio@Paulos-MacBook-Pro.local>
2017-08-31 06:19:42 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }*/
   
-  if (window.location.pathname == '{{ site.baseurl }}research' {
+  if (window.location.pathname == 'http://pauloserodio.com/research' {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
