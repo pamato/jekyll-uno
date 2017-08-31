853144932fddf94d5403809ecf86a7260b6a6835
pamato <pauloserodio@Paulos-MacBook-Pro.local>
2017-08-31 04:38:16 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -21,6 +21,10 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
   
+  if (window.location.pathname == '{{ site.url }}research'{
+    $('.panel-cover').addClass('panel-cover--collapsed') 
+  }
+  
   /*if (window.location.pathname !== '{{ site.url }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
     /*$('.panel-cover').addClass('panel-cover')
     $('.panel-cover').addClass('panel-cover--collapsed') 
